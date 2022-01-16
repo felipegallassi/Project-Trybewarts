3175b7175ad9ce6bc6b040aec71a0425e36163b9
Thiago <thiagoedusan5.11@outlook.com>
2022-01-16 20:24:32 -0300

    Requisitos 18 e 20 ok

--- script.js
+++ script.js
@@ -1,29 +1,43 @@
-window.onload = () => {
- formLogin()
- agreement()
-}
-
 function formLogin() {
-  document.querySelector("#submitLogin").addEventListener("click", () => {
-    const email = document.querySelector("#email").value;
-    const senha = document.querySelector("#senha").value;
-    
-    if (email == "tryber@teste.com" && senha == "123456") {
-      alert("Olá, Tryber!")
+  document.querySelector('#submitLogin').addEventListener('click', () => {
+    const email = document.querySelector('#email').value;
+    const senha = document.querySelector('#senha').value;
+
+    if (email === 'tryber@teste.com' && senha === '123456') {
+      alert('Olá, Tryber!');
+    } else {
+      alert('Email ou senha inválidos.');
     }
-    else {
-      alert("Email ou senha inválidos.")
-    }
-  })
+  });
 }
 
-// Requisito 18 \/
 function agreement() {
-  const checkbox = document.querySelector("#agreement");
-  const btn = document.querySelector("#submit-btn");
-  
-  btn.addEventListener("click", (element) => {
-    element.preventDefault();
-  })
+  const checkbox = document.querySelector('#agreement');
+  const btn = document.querySelector('#submit-btn');
 
+  checkbox.addEventListener('change', () => {
+    if (checkbox.checked) {
+      btn.removeAttribute('disabled');
+    }
+  });
 }
+
+function counter() {
+  const input = document.querySelector('#textarea');
+  const paragraph = document.querySelector('#counter');
+  const num = parseInt(document.querySelector('#counter').innerHTML, 10);
+
+  input.addEventListener('keyup', (pressedKey) => {
+    if (pressedKey.keyCode === 8) {
+      paragraph.innerHTML = num - input.value.length + 0;
+    } else if (pressedKey.keyCode !== 8) {
+      paragraph.innerHTML = num - input.value.length;
+    }
+  });
+}
+
+window.onload = () => {
+  formLogin();
+  counter();
+  agreement();
+};
